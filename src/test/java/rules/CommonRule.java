0e2f93d5c14de7197bc24c5147ce714ba3120f73
msmolyakov <smolyakovmaxim@gmail.com>
2016-06-17 16:09:08 +0300

    Some changes

--- src/test/java/rules/CommonRule.java
+++ src/test/java/rules/CommonRule.java
@@ -1,21 +1,28 @@
 package rules;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Screenshots;
+import com.codeborne.selenide.WebDriverProvider;
 import com.codeborne.selenide.WebDriverRunner;
 import com.google.common.io.Files;
 import org.junit.AssumptionViolatedException;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import ru.yandex.qatools.allure.annotations.Attachment;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static loggers.AllureStep.logToAllure;
+import static org.openqa.selenium.firefox.FirefoxDriver.PROFILE;
 
 public class CommonRule extends TestWatcher {
 
@@ -29,11 +36,32 @@ public class CommonRule extends TestWatcher {
     protected void starting(Description description) {
         System.out.println("START " + description);
 
-        System.setProperty("selenide.browser", "webdrivers.FirefoxCustomDriver");
-
+        Configuration.browser = MyGridFirefoxProvider.class.getName();
         open(host);
-        WebDriverRunner.getWebDriver().manage().window().fullscreen();
-        ((FirefoxDriver)getWebDriver()).getKeyboard().pressKey(Keys.F11);
+
+        getWebDriver().manage().window().maximize();
+    }
+
+    private static class MyGridFirefoxProvider implements WebDriverProvider {
+
+        @Override
+        public WebDriver createDriver(DesiredCapabilities capabilities) {
+            FirefoxProfile firefoxProfile = new FirefoxProfile();
+
+            firefoxProfile.setPreference("browser.fullscreen.autohide", false);
+            firefoxProfile.setPreference("browser.fullscreen.animateUp", 0);
+
+            capabilities.setCapability(PROFILE, firefoxProfile);
+            capabilities.setBrowserName("firefox");
+
+            try {
+                return new RemoteWebDriver(new URL("http://172.16.101.77:4444/wd/hub"), capabilities);
+            }
+            catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 
     @Override
@@ -57,6 +85,7 @@ public class CommonRule extends TestWatcher {
 
     @Override
     protected void finished(Description description) {
+        WebDriverRunner.closeWebDriver();
         System.out.println("FINISHED " + description);
     }
 
